d1716cf01b9b2e0dd35b2a4867cd845c1e21a949
elsaaeid <saidsadaoy@gmail.com>
2024-04-29 15:54:42 -0700

    deploy changes

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -51,7 +51,7 @@ class TestCityDocs(unittest.TestCase):
         self.assertIsNot(City.__doc__, None,
                          "City class needs a docstring")
         self.assertTrue(len(City.__doc__) >= 1,
-                            "City class needs a docstring")
+                "City class needs a docstring")
 
     def test_city_func_docstring(self):
         """Test for the presence of docstrings in City methods"""
@@ -59,7 +59,7 @@ class TestCityDocs(unittest.TestCase):
             self.assertIsNot(func.__doc__, None,
                              f"{func_name} method needs a docstring")
             self.assertTrue(len(func.__doc__) >= 1,
-                                f"{func_name} method needs a docstring")
+                    f"{func_name} method needs a docstring")
 
 
 class TestCity(unittest.TestCase):
