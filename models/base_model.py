ef5bb0031254ea2cbc734f16f3623d19b689d3fd
elsaaeid <saidsadaoy@gmail.com>
2024-04-29 09:32:34 +0200

    deploy changes

--- models/base_model.py
+++ models/base_model.py
@@ -6,8 +6,6 @@ from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import models
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
-
 if models.storage_type == "db":
     Base = declarative_base()
 else:
@@ -19,23 +17,19 @@ class BaseModel(Base):
     __abstract__ = True
 
     id = Column(String(60), primary_key=True)
-    created_at = Column(DateTime, default=datetime.now)
-    updated_at = Column(DateTime, default=datetime.now)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
     password = Column(String(128))
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        self.created_at = datetime.utcnow()
         self.updated_at = self.created_at
         for key, value in kwargs.items():
             if key == '__class__':
                 continue
             setattr(self, key, value)
-            if isinstance(self.created_at, str):
-                self.created_at = datetime.strptime(self.created_at, time)
-            if isinstance(self.updated_at, str):
-                self.updated_at = datetime.strptime(self.updated_at, time)
         if 'password' in kwargs:
             self.password = md5(kwargs['password'].encode()).hexdigest()
 
@@ -46,7 +40,7 @@ class BaseModel(Base):
 
     def save(self):
         """Updates the updated_at"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
@@ -57,10 +51,8 @@ class BaseModel(Base):
     def to_dict(self, secure_pwd=True):
         """Converts instance into dict format"""
         new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["created_at"] = new_dict["created_at"].strftime("%Y-%m-%dT%H:%M:%S.%f")
+        new_dict["updated_at"] = new_dict["updated_at"].strftime("%Y-%m-%dT%H:%M:%S.%f")
         new_dict["__class__"] = type(self).__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
