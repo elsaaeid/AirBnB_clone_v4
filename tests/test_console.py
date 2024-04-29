2307e7df82f93f1de9f47346f7b1cc1895e0ef8a
elsaaeid <saidsadaoy@gmail.com>
2024-04-29 15:05:32 +0200

    deploy changes

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,19 +7,29 @@ HBNBCommand = console.HBNBCommand
 
 class TestConsoleDocs(unittest.TestCase):
     """Class for testing documentation of the console"""
-    def test_pep8_equality_console(self):
-        """Test that console.py equals to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
 
-    def test_pep8_equality_test_console(self):
-        """Test that tests/test_console.py equals to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    def test_pep8_equality(self):
+        """Test that console.py and test_console.py conform to PEP8"""
+        files_to_check = ['models/console.py',
+                            'tests/test_models/test_console.py']
+        style_guide = pep8.StyleGuide()
+        total_errors = 0
+        error_messages = []
+
+        for file_path in files_to_check:
+            with self.subTest(path=file_path):
+                result = style_guide.check_files([file_path])
+                errors = result.total_errors
+
+                if errors > 0:
+                    print(f"PEP8 errors in {file_path}:")
+                    for error in result.messages:
+                        error_messages.append(f"- {error}")
+                total_errors += errors
+        if total_errors > 0:
+            error_message = f"Total PEP8 errors: {total_errors}\n"
+            error_message += "\n".join(error_messages)
+            self.fail(error_message)
 
     def test_console_module_docstring(self):
         """Test for the console.py module"""
