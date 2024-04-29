3b237f71043c507397281716c9407451c7530b44
elsaaeid <saidsadaoy@gmail.com>
2024-04-29 15:58:38 +0200

    deploy changes

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -112,13 +112,10 @@ class TestPlace(unittest.TestCase):
             self.assertTrue(hasattr(place, attr))
             attr_value = getattr(place, attr)
             if attr == "name":
-                self.assertEqual(attr_value, attributes[attr][1],
-                                f"Attribute '{attr}' does not match the expected value.")
+                self.assertEqual(attr_value, attributes[attr][1])
             else:
-                self.assertEqual(type(attr_value), attr_type,
-                                f"Attribute '{attr}' has an incorrect type.")
-                self.assertEqual(attr_value, default_value,
-                                f"Attribute '{attr}' does not match the default value.")
+                self.assertEqual(type(attr_value), attr_type)
+                self.assertEqual(attr_value, default_value)
 
     def test_place_instance(self):
         """Test if Place is an instance of the Place class"""
