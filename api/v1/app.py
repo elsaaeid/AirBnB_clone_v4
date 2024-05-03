37c5e2ab397ab7b20da4c1a3f84a29006d99ddba
elsaaeid <saidsadaoy@gmail.com>
2024-05-03 15:44:23 +0200

    deploy changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,12 +17,20 @@ port = getenv("HBNB_API_PORT", "5000")
 
 @app.teardown_appcontext
 def teardown(exception):
-    """Cleanup operations"""
+    """
+    Teardown function for the Flask 
+    application context.
+    """
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
+    """
+    Error handler for 404 status code.
+    Returns a JSON response with the message 
+    "Not found" and a status code of 404.
+    """
     return jsonify({"error": "Not found"}), 404
 
 
