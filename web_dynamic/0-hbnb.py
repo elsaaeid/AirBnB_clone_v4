bd0462c39b0441cefc4a52d764e9a8ade80438ce
elsaaeid <saidsadaoy@gmail.com>
2024-05-05 15:13:03 -0700

    deploy changes

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+import uuid
+
+
+app = Flask(__name__)
+port = 5000
+host = '0.0.0.0'
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    """tear down app context"""
+    storage.close()
+
+
+@app.route('/0-hbnb/', strict_slashes=False)
+def show_page():
+    """displays webpage
+    Returns:
+        HTML Page
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amenities = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    return render_template('0-hbnb.html',
+                           cache_id=uuid.uuid4(),
+                           states=states,
+                           amenities=amenities,
+                           places=places,
+                           users=users)
+
+if __name__ == "__main__":
+    app.run(host=host, port=port)
---
+++ web_dynamic/__init__.py
@@ -0,0 +1,4 @@
+""" class Flask """
+from flask import Flask
+
+app = Flask(__name__)
